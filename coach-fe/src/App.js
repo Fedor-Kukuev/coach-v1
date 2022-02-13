04c594ccd359ef7b32058136b1e9a0130d2e2270
Fedor-Kukuev <79315352+Fedor-Kukuev@users.noreply.github.com>
2022-02-12 22:10:29 -0500

    Add files via upload

---
+++ coach-fe/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://icon-library.com/images/gym-icon-png/gym-icon-png-20.jpg" className="App-logo" alt="logo" />
+        <p>
+          Coach APP v.1
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get started!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
